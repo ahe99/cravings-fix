5fb3cc61537c3242c4ebe1992a6f31a1d06ae0d5
River <ahe794613@gmail.com>
2023-06-11 16:14:29 +0800

    abstract file upload to middleware

--- src/routes/banners.js
+++ src/routes/banners.js
@@ -1,27 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const banners = require('../controller/banners')
-
-const path = require('path')
-const multer = require('multer')
-
-const MAX_FILE_SIZE = 1024 // 1mb
-const EXT_CHECK = ['.webp', '.jpg', '.png', '.jpeg', '.bmp', '.gif']
-
-const upload = multer({
-  limit: {
-    fileSize: MAX_FILE_SIZE,
-  },
-  fileFilter: function (req, file, callback) {
-    const ext = path.extname(file.originalname)
-
-    if (EXT_CHECK.indexOf(ext) === -1) {
-      req.fileValidationError = 'FILE_NOT_ALLOWED'
-      return callback()
-    }
-    callback(null, true)
-  },
-})
+const { upload } = require('../middleware/file')
 
 router.get('/', banners.getAllBanners)
 router.get('/:id', banners.getSingleBanner)
--- src/routes/food.js
+++ src/routes/food.js
@@ -9,26 +9,7 @@ const {
   PatchFoodRequestSchema,
   DeleteFoodRequestSchema,
 } = require('../schema/food')
-const path = require('path')
-const multer = require('multer')
-
-const MAX_FILE_SIZE = 1024 // 1mb
-const EXT_CHECK = ['.webp', '.jpg', '.png', '.jpeg', '.bmp', '.gif']
-
-const upload = multer({
-  limit: {
-    fileSize: MAX_FILE_SIZE,
-  },
-  fileFilter: function (req, file, callback) {
-    const ext = path.extname(file.originalname)
-
-    if (EXT_CHECK.indexOf(ext) === -1) {
-      req.fileValidationError = 'FILE_NOT_ALLOWED'
-      return callback()
-    }
-    callback(null, true)
-  },
-})
+const { upload } = require('../middleware/file')
 
 router.get('/', validateResource(GetFoodsRequestSchema), food.getAllFoods)
 router.get('/:id', validateResource(GetFoodRequestSchema), food.getSingleFood)
