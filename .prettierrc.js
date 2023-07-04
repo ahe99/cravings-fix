c5e58342ba3c8f9bd54ba754dbb7639d4f5e7710
River <ahe794613@gmail.com>
2023-07-04 11:52:18 +0800

    init commit for migrating monorepo

---
+++ .prettierrc.js
@@ -0,0 +1,25 @@
+
+/** @type {import("prettier").Options} */
+const config = {
+  printWidth: 80,
+
+  tabWidth: 2,
+  useTabs: false,
+
+  singleQuote: true,
+  quoteProps: 'consistent',
+
+  semi: false,
+  trailingComma: 'all',
+
+  bracketSpacing: true,
+  arrowParens: 'always',
+
+  rangeStart: 0,
+  rangeEnd: Number.POSITIVE_INFINITY,
+
+  endOfLine: 'lf',
+
+};
+
+module.exports = config;
